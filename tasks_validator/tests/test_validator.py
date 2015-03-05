376b7137626aeda2da4909f0dafcf959f85589c5
Kamil Sambor <ksambor@mirantis.com>
2015-03-05 15:53:04 +0100

    Create tasks validator
    * create graph validator
    * create tasks validator to validate schema and graph
      based on this tasks
    * add test
    
    Change-Id: I522e6622475922cd6a7f501d31ec6881ecc6b202

---
+++ tasks_validator/tests/test_validator.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import jsonschema
+
+from unittest2.case import TestCase
+
+from tasks_validator import validator
+
+
+class TestValidator(TestCase):
+
+    def setUp(self):
+        self.tasks = [
+            {'id': 'pre_deployment_start',
+             'type': 'stage'},
+            {'id': 'pre_deployment_end',
+             'requires': ['pre_deployment_start'],
+             'type': 'stage'},
+            {'id': 'deploy_start',
+             'requires': ['pre_deployment_end'],
+             'type': 'stage'},
+            {'id': 'deploy_end',
+             'requires': ['deploy_start'],
+             'type': 'stage'},
+            {'id': 'post_deployment_start',
+             'requires': ['deploy_end'],
+             'type': 'stage'},
+            {'id': 'post_deployment_end',
+             'requires': ['post_deployment_start'],
+             'type': 'stage'}]
+
+    def test_validate_schema(self):
+        valid_tasks = validator.TasksValidator(self.tasks, "6.1")
+        valid_tasks.validate_schema()
+
+    def test_wrong_schema(self):
+        self.tasks.append({'id': 'wrong',
+                           'type': 'non existing'})
+        valid_tasks = validator.TasksValidator(self.tasks, "6.1")
+        self.assertRaises(jsonschema.ValidationError,
+                          valid_tasks.validate_schema)
+
+    def test_validate_graph(self):
+        valid_tasks = validator.TasksValidator(self.tasks, "6.1")
+        valid_tasks.validate_graph()
+
+    def test_validate_cyclic_graph(self):
+        self.tasks.append({'id': 'post_deployment_part',
+                           'type': 'stage',
+                           'requires': ['post_deployment_start'],
+                           'required_for': ['pre_deployment_start']})
+        valid_tasks = validator.TasksValidator(self.tasks, "6.1")
+        self.assertRaises(ValueError,
+                          valid_tasks.validate_graph)
+
+    def test_validate_not_connected_graph(self):
+        self.tasks.append({'id': 'post_deployment_part',
+                           'type': 'stage'})
+        valid_tasks = validator.TasksValidator(self.tasks, "6.1")
+        self.assertRaises(ValueError,
+                          valid_tasks.validate_graph)
---
+++ tasks_validator/validator.py
@@ -0,0 +1,54 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import jsonschema
+
+import graph
+from schemas import VERSIONS_SCHEMAS_MAP
+
+
+class TasksValidator(object):
+
+    def __init__(self, tasks, version):
+        self.version = version
+        self.tasks = tasks
+        self.graph = graph.DeploymentGraph(tasks)
+
+    def validate_schema(self):
+        """Validate tasks schema
+
+        :raise: jsonschema.ValidationError
+        """
+        checker = jsonschema.FormatChecker()
+        schema = VERSIONS_SCHEMAS_MAP.get(self.version)().tasks_schema
+        jsonschema.validate(self.tasks, schema, format_checker=checker)
+
+    def validate_graph(self):
+        """Validate graph if is executable completely by fuel nailgun
+
+        :raise: ValueEror when one of requirements is not satisfied
+        """
+        msgs = []
+
+        # deployment graph should be without cycles
+        cycles = self.graph.find_cycles()
+        if len(cycles):
+            msgs.append('Graph is not acyclic. Cycles: {0}'.format(cycles))
+
+        # graph should be connected to execute all tasks
+        if not self.graph.is_connected():
+            msgs.append('Graph is not connected.')
+
+        if msgs:
+            raise ValueError("Graph validation fail: {0}".format(msgs))
