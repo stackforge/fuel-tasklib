d82c98b725f55a47e0d3beb315199a804dcff263
Kamil Sambor <ksambor@mirantis.com>
2015-03-11 16:57:26 +0100

    Modified graph to be usable in nailgun
    * add new methods to graph
    * migrate tests
    
    Change-Id: I2b050ad1fe58376107ac8a2d6b7e021de3bc43ef

--- tasks_validator/graph.py
+++ tasks_validator/graph.py
@@ -15,38 +15,51 @@
 
 import networkx as nx
 
+from tasks_validator import consts
 
-class DeploymentGraph(object):
 
-    def __init__(self, tasks):
-        self.tasks = tasks
-        self.graph = self._create_graph()
+class DeploymentGraph(nx.DiGraph):
+    """DirectedGraph that is used to generate configuration for orchestrator.
 
-    def _create_graph(self):
-        """Create graph from tasks
+    General task format
 
-        :return: directed graph
-        """
-        graph = nx.DiGraph()
-        for task in self.tasks:
-            task_id = task.get('id')
-            if task_id:
-                graph.add_node(task_id, **task)
-                if 'required_for' in task:
-                    for req in task['required_for']:
-                        graph.add_edge(task_id, req)
-                if 'requires' in task:
-                    for req in task['requires']:
-                        graph.add_edge(req, task_id)
+    id: string
+    type: string - one of - role, stage, puppet, shell, upload_file, sync
+    required_for: direct dependencies
+    requires: reverse dependencies
 
-                if 'groups' in task:
-                    for req in task['groups']:
-                        graph.add_edge(task_id, req)
-                if 'tasks' in task:
-                    for req in task['tasks']:
-                        graph.add_edge(req, task_id)
+    groups: direct dependencies for different levels
+    tasks: reverse dependencies for different levels
 
-        return graph
+    stage: direct dependency
+    parameters: specific for each task type parameters
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(DeploymentGraph, self).__init__(*args, **kwargs)
+
+    def add_tasks(self, tasks):
+        for task in tasks:
+            self.add_task(task)
+
+    def add_task(self, task):
+        task_id = task.get('id')
+        if task_id:
+            self.add_node(task_id, **task)
+            if 'required_for' in task:
+                for req in task['required_for']:
+                    self.add_edge(task_id, req)
+            if 'requires' in task:
+                for req in task['requires']:
+                    self.add_edge(req, task_id)
+
+            if 'groups' in task:
+                for req in task['groups']:
+                    self.add_edge(task_id, req)
+            if 'tasks' in task:
+                for req in task['tasks']:
+                    self.add_edge(req, task_id)
 
     def find_cycles(self):
         """Find cycles in graph.
@@ -54,9 +67,8 @@ class DeploymentGraph(object):
         :return: list of cycles in graph
         """
         cycles = []
-        for cycle in nx.simple_cycles(self.graph):
+        for cycle in nx.simple_cycles(self):
             cycles.append(cycle)
-
         return cycles
 
     def is_connected(self):
@@ -64,4 +76,91 @@ class DeploymentGraph(object):
 
         :return: bool
         """
-        return nx.is_weakly_connected(self.graph)
+        return nx.is_weakly_connected(self)
+
+    def get_next_groups(self, processed_nodes):
+        """Get nodes that have predecessors in processed_nodes list.
+
+        All predecessors should be taken into account, not only direct
+        parents
+
+        :param processed_nodes: set of nodes names
+        :returns: list of nodes names
+        """
+        result = []
+        for node in self.nodes():
+            if node in processed_nodes:
+                continue
+
+            predecessors = nx.dfs_predecessors(self.reverse(), node)
+            if (set(predecessors.keys()) <= processed_nodes):
+                result.append(node)
+
+        return result
+
+    def get_groups_subgraph(self):
+        roles = [t['id'] for t in self.node.values()
+                 if t['type'] == consts.ORCHESTRATOR_TASK_TYPES.group]
+        return self.subgraph(roles)
+
+    def get_tasks(self, group_name):
+        tasks = []
+        for task in self.predecessors(group_name):
+            if self.node[task]['type'] not in consts.INTERNAL_TASKS:
+                tasks.append(task)
+        return self.subgraph(tasks)
+
+    @property
+    def topology(self):
+        return map(lambda t: self.node[t], nx.topological_sort(self))
+
+    def make_void_task(self, task):
+        """Make some task in graph simple void
+
+        We can not just remove node because it also stores edges, that connects
+        graph in correct order
+
+        :param task_id: id of the node in graph
+        """
+        if task['type'] in consts.INTERNAL_TASKS:
+            return
+
+        task['type'] = consts.ORCHESTRATOR_TASK_TYPES.void
+
+    def only_tasks(self, task_ids):
+        """Leave only tasks that are specified in request.
+
+        :param task_ids: list of task ids
+        """
+        if not task_ids:
+            return
+
+        for task in self.node.values():
+            if task.get('id') not in task_ids:
+                self.make_void_task(task)
+
+    def find_subgraph(self, start=None, end=None):
+        """Find subgraph by provided start and end endpoints
+
+        :param end: string
+        :param start: string
+        :returns: DeploymentGraph instance (subgraph from original)
+        """
+        working_graph = self
+
+        if start:
+            # simply traverse starting from root,
+            # A->B, B->C, B->D, C->E
+            working_graph = self.subgraph(
+                nx.dfs_postorder_nodes(working_graph, start))
+
+        if end:
+            # nx.dfs_postorder_nodes traverses graph from specified point
+            # to the end by following successors, here is example:
+            # A->B, C->D, B->D , and we want to traverse up to the D
+            # for this we need to reverse graph and make it
+            # B->A, D->C, D->B and use dfs_postorder
+            working_graph = self.subgraph(nx.dfs_postorder_nodes(
+                working_graph.reverse(), end))
+
+        return working_graph
